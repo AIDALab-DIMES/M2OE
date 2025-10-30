c90ca32bff92efcc865640ecabadaf4101280342
Simona Nisticò <50133767+simona-nistico@users.noreply.github.com>
2025-10-30 19:48:19 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 setuptools.setup(
     name="M2OE",
-    version="0.0.1",
+    version="0.0.2",
     author="Fabrizio Angiulli, Fabio Fassetti, Simona Nisticò, Luigi Palopoli",
     author_email="simona.nistico@dimes.unical.it",
     description="Masking Models for Outlier Explanation",
